cb805e80e60ab8a9ae3f559d27cc2f9e9a92d652
Hassan-El-Sobky <hassan_elsobky@hotmail.com>
2022-06-10 17:52:54 +0200

    Sales-team-table-Done 👌

--- app/components/employees-list/employees-list.component.js
+++ app/components/employees-list/employees-list.component.js
@@ -9,4 +9,18 @@ angular
     },
   });
 
-function EmployeesListComponent() {}
+function EmployeesListComponent($scope,$location,$rootScope,$sce) {
+    $scope.filter='';
+      $rootScope.$on('$locationChangeSuccess', function(event){
+     $scope.filter=$location.search().filter;
+          console.log($scope.filter);
+})
+      console.log($location);
+    $scope.highlight = function(text) {
+    if (!$location.$$search.filter) {
+        return $sce.trustAsHtml(text);
+    }
+    // console.log(text);
+      return $sce.trustAsHtml(text.replace(new RegExp($location.$$search.filter, 'gi'), '<span class="highlightedText">$&</span>'));
+};
+}
---
+++ app/components/vue-components/FilterComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div >
+    <input class="c-search__input" type="text" name="filter" v-model="filter" placeholder="Search...">
+    <button class="c-button" @click="clearInput()">Clear</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'FilterComponent',
+        data(){
+              return {
+                filter:''
+              }
+        },
+        methods: {
+            clearInput(){
+                this.filter=''
+            },
+        },
+         watch: {
+      filter() {
+      this.$emit('filter',this.filter);
+    }
+  },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
